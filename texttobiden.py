9a3f9aa4990fb66d90631ad2e882247d0c9cd917
OshDubh <oshdubh@gmail.com>
2023-02-18 23:12:54 +0000

    help function now retrieves the docstring

--- texttobiden.py
+++ texttobiden.py
@@ -11,35 +11,36 @@ import os
 import sys
 
 def help(command_list):
-    """returns all commands, or gives info on a specific command"""
-    # variables
-    commands = {
-        "help": "Get general info on the available commands.",
-        "init": "Initialise a new project",
-        "image": "Process new, and give tags to, google images.",
-        "music": "Adds a background music track to a given video",
-    }
+	"""Get general info on the available commands."""
+	# variables
+	commands = {
+		"help": help.__doc__,
+		"init": "Initialise a new project.",
+		"setup": "Instructions for performing first-time setup of text-to-biden.",
+		"image": "Process new, and give tags to, google images.",
+		"music": "Adds a background music track to a given video.",
+	}
 
-    output = "" # the built string that this function returns
+	output = "" # the built string that this function returns
 
-    # output all of the available commands, since no specific command was supplied
-    if not command_list:
-        output += "Available commands:\n"
+	# output all of the available commands, since no specific command was supplied
+	if not command_list:
+		output += "Available commands:\n"
 
-        for command in commands:
-            output = output + " - " + command + "\n"
+		for command in commands:
+			output = output + " - " + command + "\n"
 
-        output = output + "Type 'help [command]' to get more info on a specific command.\n"
+		output = output + "Type 'help [command]' to get more info on a specific command.\n"
 
-    # give the info on a given command, if the given option was a command
-    else:
-        for s in command_list:
-            if s in commands:
-                output = output + s + ": " + commands[s] + "\n"
-            else:
-                output = output + '"' + s + '"' + " is not a valid command. Use [help] to see a list of all the available commands.\n"
+	# give the info on a given command, if the given option was a command
+	else:
+		for s in command_list:
+			if s in commands:
+				output = output + s + ": " + commands[s] + "\n"
+			else:
+				output = output + '"' + s + '"' + " is not a valid command. Use [help] to see a list of all the available commands.\n"
 
-    return output # pass the built string back out
+	return output # pass the built string back out
 
 
 print(help(sys.argv[1:]))
