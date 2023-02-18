f9c9f8ff8e370a3225ba4af892ee2e23e36dd312
OshDubh <oshdubh@gmail.com>
2023-02-18 23:24:01 +0000

    starting to move help.py to a independent file

---
+++ help.py
@@ -0,0 +1,7 @@
+#!/usr/env/bin python3
+
+# filename: help.py
+# author: OshDubh
+# description: gets a list of all of the local functions, then lists them and their docstrings to build a complete help functio
+# date created: 23:23 on the 18th of February, 2023
+# date last modified: 23:23 on the 18th of February, 2023
