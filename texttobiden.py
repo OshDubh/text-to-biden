4038764d9ea76f8c8f33f27cd14f732ce4b7b972
OshDubh <oshdubh@gmail.com>
2023-02-18 22:27:30 +0000

    intial main file commit

---
+++ texttobiden.py
@@ -0,0 +1,8 @@
+#!/usr/env/bin python3
+
+# filename: texttobiden.py
+# author: OshDubh
+# description: the main file for the texttobiden program, the one that should be run to create content
+# date created: 22:26 on the 18th of February, 2023
+# date last modified: 22:26 on the 18th of February, 2023
+
