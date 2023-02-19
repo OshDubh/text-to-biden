8e5086dab75e0250d1acfaf8f3c20ea2a981944c
OshDubh <oshdubh@gmail.com>
2023-02-19 11:17:49 +0000

    completed moving help to its own file

--- help.py
+++ help.py
@@ -5,3 +5,35 @@
 # description: gets a list of all of the local functions, then lists them and their docstrings to build a complete help functio
 # date created: 23:23 on the 18th of February, 2023
 # date last modified: 23:23 on the 18th of February, 2023
+
+def help(command_list):
+	"""Get general info on the available commands."""
+	# variables
+	commands = {
+		"help": help.__doc__,
+		"init": "Initialise a new project.",
+		"setup": "Instructions for performing first-time setup of text-to-biden.",
+		"image": "Process new, and give tags to, google images.",
+		"music": "Adds a background music track to a given video.",
+	}
+
+	output = "" # the built string that this function returns
+
+	# output all of the available commands, since no specific command was supplied
+	if not command_list:
+		output += "Available commands:\n"
+
+		for command in commands:
+			output = output + " - " + command + "\n"
+
+		output = output + "Type 'help [command]' to get more info on a specific command.\n"
+
+	# give the info on a given command, if the given option was a command
+	else:
+		for s in command_list:
+			if s in commands:
+				output = output + s + ": " + commands[s] + "\n"
+			else:
+				output = output + '"' + s + '"' + " is not a valid command. Use [help] to see a list of all the available commands.\n"
+
+	return output # pass the built string back out
--- texttobiden.py
+++ texttobiden.py
@@ -6,43 +6,11 @@
 # date created: 22:26 on the 18th of February, 2023
 # date last modified: 22:26 on the 18th of February, 2023
 
-# imports
+# external imports
 import os
 import sys
 
-def help(command_list):
-	"""Get general info on the available commands."""
-	# variables
-	commands = {
-		"help": help.__doc__,
-		"init": "Initialise a new project.",
-		"setup": "Instructions for performing first-time setup of text-to-biden.",
-		"image": "Process new, and give tags to, google images.",
-		"music": "Adds a background music track to a given video.",
-	}
+# local imports
+from help import help
 
-	output = "" # the built string that this function returns
-
-	# output all of the available commands, since no specific command was supplied
-	if not command_list:
-		output += "Available commands:\n"
-
-		for command in commands:
-			output = output + " - " + command + "\n"
-
-		output = output + "Type 'help [command]' to get more info on a specific command.\n"
-
-	# give the info on a given command, if the given option was a command
-	else:
-		for s in command_list:
-			if s in commands:
-				output = output + s + ": " + commands[s] + "\n"
-			else:
-				output = output + '"' + s + '"' + " is not a valid command. Use [help] to see a list of all the available commands.\n"
-
-	return output # pass the built string back out
-
-
-print(help(sys.argv[1:]))
-
-print(dir("./"))
+print(help(sys.argv))
