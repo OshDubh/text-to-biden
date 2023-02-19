6e51d9a76d05d9a8052e1f559c36405760d6aef5
OshDubh <oshdubh@gmail.com>
2023-02-19 12:18:57 +0000

    added selection of a folder if it already exists

--- init.py
+++ init.py
@@ -4,7 +4,7 @@
 # author: OshDubh
 # description: Initialises a new project folder
 # date created: 11:19 on the 19th of February, 2023
-# date last modified: 12:09 on the 19th of February, 2023
+# date last modified: 12:17 on the 19th of February, 2023
 
 # external imports
 import os
@@ -16,12 +16,15 @@ def init(name):
 		os.mkdir('./Projects')
 
 	name = name[0] # it's passed as a list, so convert the first arg to string 
-	# create a new project folder with the given name(s)
+	# create a new project folder with the given name
+	path = os.path.join('./Projects', name)
+
 	# catch if the file already exists
 	try:
-		path = os.path.join('./Projects', name)
 		os.mkdir(path)
 		print(path, "was created.")
 		return path # pass the path back to the parent process so we know where to work
+
 	except FileExistsError:
-		return "Project folder already exists."
+		print("Project folder already exists.", name, "was selected as the current project folder")
+		return path # pass the path back 
